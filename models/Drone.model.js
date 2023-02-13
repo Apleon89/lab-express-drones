eb9eaf53c1edc62cd28f408861fce92005f46796
Alberto Pérez <apleon89@gmail.com>
2023-02-13 16:02:19 +0000

    iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+
+const { Schema, model } = require('mongoose');
+
+const droneModel = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+const DroneModel = model('Drones', droneModel)
+
+module.exports = DroneModel
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+require('../db/index.js')
+
+const DroneModel = require('../models/Drone.model.js')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+const insertData = async () => {
+
+    try {
+        const response = await DroneModel.create(drones)
+        console.log('Drones added')
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+insertData()
