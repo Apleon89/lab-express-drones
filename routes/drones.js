490d30d25090cf4811038ca003f553e763de6276
Alberto Pérez <apleon89@gmail.com>
2023-02-13 16:35:39 +0000

    iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -20,12 +20,24 @@ router.get('/drones', async (req, res, next) => {
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  
+  const { name, propellers, maxSpeed} = req.body
+  try {
+    const newDrone = await DroneModel.create({
+      name: name,
+      propellers: propellers,
+      maxSpeed: maxSpeed
+    })
+    res.redirect('/drones')
+  }
+  catch (err) {
+    res.redirect('/drones/create')
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
